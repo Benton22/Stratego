23b27f1588c5918cc1d653691ab08f32f4599f06
Benton22 <thebenton22@gmail.com>
2024-02-13 09:36:09 -0600

    Grid overlay that can have a background put under

--- Main.py
+++ Main.py
@@ -7,6 +7,12 @@ from Screens.Board import BoardScreen
 from Screens.Menu import MenuScreen
 from Screens.Logic.Buttons import BasicButton
 
+height = globalheight
+width = globalwidth
+size = globalsize
+black = 0, 0, 0
+background = globalbackground
+white = 255, 255, 255
 
 ScreenIndex = [1]
 
@@ -15,15 +21,8 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
-    height = 1000
-    width = height * 10 / 12
-    size = width, height
-    black = 0, 0, 0
-    background = 43,43,43
-    white = 255, 255, 255
+    
     screen = pygame.display.set_mode(size)
-
 
     if ScreenIndex[0] == 1: MenuScreen(screen, ScreenIndex)
     elif ScreenIndex[0] == 2: BoardScreen(screen, ScreenIndex)
--- Screens/Board.py
+++ Screens/Board.py
@@ -8,8 +8,34 @@ size = globalsize
 black = 0, 0, 0
 background = globalbackground
 white = 255, 255, 255
-gridSize = 10
+gridSize = 11
 gridLinesH = []
+gridLinesV = []
+gridLineFiller = []
+
+
+class GridLines():
+    def __init__(self, locationX, locationY, sizeX, sizeY):
+        self.sizeX = sizeX
+        self.sizeY = sizeY
+        self.locationX = locationX
+        self.locationY = locationY
+        self.Bezel = 1
+        self.outerLine = pygame.Rect(self.locationX, self.locationY, self.sizeX, self.sizeY)
+        self.innerLine = pygame.Rect(self.locationX + self.Bezel, self.locationY + self.Bezel, sizeX - 2 * self.Bezel, sizeY - 2 * self.Bezel)
+
+#standard grid
+for i in range(0, gridSize):
+    gridLinesH.append (GridLines(0, (i * height/12) + height/12-3, width, 6))
+    gridLinesV.append (GridLines((i * height/12) + height/12, height/12, 6, width))
+#filling in the gaps
+gridLineFiller.append(GridLines(0, height/2, 2 * width/10 + 3, 6))
+gridLineFiller.append(GridLines(4 * width/10 + 3, height/2, 2 * width/10, 6))
+gridLineFiller.append(GridLines(8 * width/10 + 3, height/2, 2 * width/10, 6))
+gridLineFiller.append(GridLines((3 * height/12), height/12, 6, 4* height/12))
+gridLineFiller.append(GridLines((7 * height/12), height/12, 6, 4* height/12))
+gridLineFiller.append(GridLines((3 * height/12), 6 * height/12 + height/12, 6, 4* height/12))
+gridLineFiller.append(GridLines((7 * height/12), 6 * height/12 + height/12, 6, 4* height/12))
 
 def BoardScreen(screen, CurrentScreenIndex):
     running = True
@@ -20,23 +46,26 @@ def BoardScreen(screen, CurrentScreenIndex):
                 pygame.quit()
                 sys.exit()
 
-        # Your game logic goes here
-        class GridLines():
-            def __init__(self, locationX, locationY, sizeX, sizeY):
-                self.hovering = False
-                self.sizeX = sizeX
-                self.sizeY = sizeY
-                self.locationX = locationX
-                self.locationY = locationY
-                self.Bezel = sizeX/20
-                self.outerButton = pygame.Rect(self.locationX, self.locationY, self.sizeX, self.sizeY)
-                self.innerButton = pygame.Rect(self.locationX + self.Bezel, self.locationY + self.Bezel, sizeX - 2 * self.Bezel, sizeY - 2 * self.Bezel)
-        
-        for i in range(0, gridSize):
-            gridLinesH [i] = GridLines(0, gridlines)
 
         # Drawing
         screen.fill(background)
+        for gridLine in range(len(gridLinesH)):
+            if gridLine != 5:
+                pygame.draw.rect(screen, black, gridLinesH[gridLine].outerLine)
+            if gridLine != 2 and gridLine != 6:
+                pygame.draw.rect(screen, black, gridLinesV[gridLine].outerLine)
+
+        for gridLine in range(len(gridLineFiller)):
+            pygame.draw.rect(screen, black, gridLineFiller[gridLine].outerLine)
+
+        for gridLine in range(len(gridLinesH)):
+            if gridLine != 5:
+                pygame.draw.rect(screen, white, gridLinesH[gridLine].innerLine)
+            if gridLine != 2 and gridLine != 6:
+                pygame.draw.rect(screen, white, gridLinesV[gridLine].innerLine)
+
+        for gridLine in range(len(gridLineFiller)):
+            pygame.draw.rect(screen, white, gridLineFiller[gridLine].innerLine)
 
         # Update display
         pygame.display.flip()
